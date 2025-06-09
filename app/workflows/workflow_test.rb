a30898d5709090b037c86c6902d873404611010a
Ignacio Flores <inifl99@gmail.com>
2025-06-09 15:46:55 -0300

    rails app w postgres ruby llm and roast

---
+++ app/workflows/workflow_test.rb
@@ -0,0 +1,19 @@
+require "roast"
+require "bundler/setup"
+require "roast/workflow/base_step"
+require "roast/workflow/prompt_step"
+require "roast/workflow/base_iteration_step"
+require "roast/workflow/repeat_step"
+require "roast/workflow/each_step"
+require "roast/workflow/base_workflow"
+require "roast/workflow/configuration"
+require "roast/workflow/workflow_execution_context"
+require "roast/workflow/workflow_executor"
+require "roast/workflow/configuration_parser"
+require "roast/workflow/validator"
+require "roast/workflow/state_repository"
+require "roast/workflow/session_manager"
+require "roast/workflow/file_state_repository"
+class WorkflowTest
+  include Roast::Workflow
+end
---
+++ start.sh
@@ -0,0 +1,2 @@
+rm -f tmp/pids/server.pid
+bundle exec rails s -b 0.0.0.0
